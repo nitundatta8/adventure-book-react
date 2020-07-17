06b8b9ce2c3f494f89018077a20e7605261d9215
nitunwa <nitun.2@gmail.com>
2020-07-17 15:26:52 -0700

    add initial template design

--- src/components/AdventuresControl.js
+++ src/components/AdventuresControl.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { section, MenuItem } from 'react-bootstrap';
+import { Image } from 'react-bootstrap'
+
 class AdventuresControl extends React.Component {
   constructor(props) {
     super(props);
@@ -13,12 +14,30 @@ class AdventuresControl extends React.Component {
         <section class="gallery-block cards-gallery">
           <div class="container">
             <div class="heading">
-              <h2>Cards Gallery</h2>
+              <h2>Gallery</h2>
             </div>
             <div class="row">
               <div class="col-md-6 col-lg-4">
                 <div class="card border-0 transform-on-hover">
-
+                  <Image src="img/image3.jpg" thumbnail />
+                  <div class="card-body">
+                    <h6><a href="#">Lorem Ipsum</a></h6>
+                    <p class="text-muted card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6 col-lg-4">
+                <div class="card border-0 transform-on-hover">
+                  <Image src="img/image4.jpg" thumbnail />
+                  <div class="card-body">
+                    <h6><a href="#">Lorem Ipsum</a></h6>
+                    <p class="text-muted card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6 col-lg-4">
+                <div class="card border-0 transform-on-hover">
+                  <Image src="img/image5.jpg" thumbnail />
                   <div class="card-body">
                     <h6><a href="#">Lorem Ipsum</a></h6>
                     <p class="text-muted card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.</p>
@@ -29,6 +48,7 @@ class AdventuresControl extends React.Component {
             </div>
           </div>
         </section>
+
 
 
       </React.Fragment>

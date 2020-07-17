65825bce3a90d7eb0b2a43c8fb5d193a66d5ae59
nitunwa <nitun.2@gmail.com>
2020-07-17 08:27:16 -0700

    add initial commit for project adventure-book

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Adventure Book</h2>
+    </div>
+  );
+}
+
+export default App;

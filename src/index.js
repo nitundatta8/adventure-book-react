11fcf3d0a215ca8dc1c9bd26f7f6fce7b0ecdf4b
nitunwa <nitun.2@gmail.com>
2020-07-24 16:23:28 -0700

    add thunk middleware to make a api cal

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import middlewareLogger from './middleware/middleware-logger';
+import rootReducer from './reducers/index'
+
+const store = createStore(rootReducer, applyMiddleware(middlewareLogger, thunkMiddleware));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 

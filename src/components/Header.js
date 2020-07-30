04846466fe4344d30c1f24666d4409e0ee5c22fd
nitunwa <nitun.2@gmail.com>
2020-07-30 12:27:38 -0700

    fix header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-
+import { Navbar, Nav, Container } from 'react-bootstrap';
 function Header(props) {
   const styleHeader = {
     display: 'flex',
@@ -22,12 +22,29 @@ function Header(props) {
 
 
   return (
-    <div style={styleHeader}>
-      <h3>Adventure Book</h3>
+    <Container>
+      <div >
+        {/* <h3>Adventure Book</h3>
       <Link style={link} to="/signin"><h4>Sign In</h4></Link>
       <Link style={link} to="/"><h4>Home</h4></Link>
-      <Link style={link} to="/newAdventureForm"><h4>Share Adventure</h4></Link>
-    </div>
+      <Link style={link} to="/newAdventureForm"><h4>Share Adventure</h4></Link> */}
+
+        <Navbar bg="light" expand="lg" className="bg-light justify-content-lg-center">
+          <Navbar.Brand href="/">Adventure Book</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/signin">Sign In</Nav.Link>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/newAdventureForm">Share Adventure</Nav.Link>
+
+            </Nav>
+
+          </Navbar.Collapse>
+        </Navbar>
+
+      </div >
+    </Container>
   );
 }
 

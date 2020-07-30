48ad00f4bba45169b1acc3736b7a88333cda4df6
nitunwa <nitun.2@gmail.com>
2020-07-30 12:01:44 -0700

    add functionality to redirect to the product url and display user name next to comment

--- src/components/ProductTag.js
+++ src/components/ProductTag.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const ProductTag = (props) => {
-  const { productUrl } = props;
+  const { productUrl, category } = props;
 
   const tagStyle = {
     left: (props.x - 13).toString() + 'px',
@@ -13,11 +13,22 @@ const ProductTag = (props) => {
     color: 'white'
   };
 
+  const getCommision = (event) => {
+    event.preventDefault();
+    console.log(" get commition");
+    window.open(productUrl, "_blank");
+  };
+  const cursorStyle = {
+    cursor: 'pointer',
+    color: 'white'
+  }
+
   return (
     <>
 
       <div id={"tagged" + props.id} className="tag" style={tagStyle} >
-        <span id="span-id"><a href={productUrl} target="_blank" style={productLinkCss}>{props.productName}</a></span>
+        {/* <span id="span-id"><a href={productUrl} target="_blank" style={productLinkCss}>{props.productName}</a></span> */}
+        <span ><a href="#" style={cursorStyle} onClick={getCommision} data-placement="top" title={props.productName}>{props.category}</a></span>
       </div>
     </>
 

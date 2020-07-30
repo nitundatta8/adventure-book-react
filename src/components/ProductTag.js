04f37e39384ba9fac5d501821b4ba2d0ec0a9898
nitunwa <nitun.2@gmail.com>
2020-07-29 23:58:49 -0700

    display tagging on loading page

--- src/components/ProductTag.js
+++ src/components/ProductTag.js
@@ -2,17 +2,22 @@ import React from 'react';
 
 
 const ProductTag = (props) => {
+  const { productUrl } = props;
+
   const tagStyle = {
     left: (props.x - 13).toString() + 'px',
     top: (props.y - 13).toString() + 'px',
     visibility: "visible"
   }
+  const productLinkCss = {
+    color: 'white'
+  };
 
   return (
     <>
 
       <div id={"tagged" + props.id} className="tag" style={tagStyle} >
-        <span id="span-id">{props.productName}</span>
+        <span id="span-id"><a href={productUrl} target="_blank" style={productLinkCss}>{props.productName}</a></span>
       </div>
     </>
 
